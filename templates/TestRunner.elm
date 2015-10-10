72aed5c9dbca02845acb8d2cbee597ec3f2d53cf
Aaron VonderHaar <gruen0aermel@gmail.com>
2015-10-10 15:01:14 -0700

    Update for Elm 0.16

--- templates/TestRunner.elm
+++ templates/TestRunner.elm
@@ -3,16 +3,13 @@ module Main where
 import Signal exposing (Signal)
 
 import ElmTest.Runner.Console exposing (runDisplay)
-import IO.IO exposing (IO)
-import IO.Runner exposing (Request, Response)
-import IO.Runner as Run
+import Console exposing (IO)
+import Task
 
 import Tests
 
 console : IO ()
 console = runDisplay Tests.all
 
-port requests : Signal Request
-port requests = Run.run responses console
-
-port responses : Signal Response
+port runner : Signal (Task.Task x ())
+port runner = Console.run console
