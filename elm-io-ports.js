e4b20ba7152b63c4117e6e9297382d1912506a09
Richard Feldman <richard.t.feldman@gmail.com>
2015-10-18 11:47:04 -0700

    Re-fix the Date bug originally fixed by 603c535c01beeb912c2057da05e171ba6f7c5c6b

--- elm-io-ports.js
+++ elm-io-ports.js
@@ -1,6 +1,7 @@
 /* Implementation from: https://raw.githubusercontent.com/laszlopandy/elm-console/1.0.2/elm-io.sh */
 module.exports =
   "(function(){\n" +
+  "    window = {Date: Date};\n" +
   "    if (typeof Elm === \"undefined\") { throw \"elm-io config error: Elm is not defined. Make sure you call elm-io with a real Elm output file\"}\n" +
   "    if (typeof Elm.Main === \"undefined\" ) { throw \"Elm.Main is not defined, make sure your module is named Main.\" };\n" +
   "    var worker = Elm.worker(Elm.Main);\n" +
