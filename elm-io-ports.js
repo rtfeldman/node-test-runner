9071c67f9b9eb3d5f600bda9d915bfabfae7d86e
Aaron VonderHaar <gruen0aermel@gmail.com>
2016-01-16 10:01:10 -0800

    Add addEventListener, removeEventListener to the window stub

--- elm-io-ports.js
+++ elm-io-ports.js
@@ -1,7 +1,7 @@
 /* Implementation from: https://github.com/laszlopandy/elm-console/blob/master/elm-io.sh */
 module.exports =
   "(function(){\n" +
-  "    window = {Date: Date};\n" +
+  "    window = {Date: Date,addEventListener : function() {},removeEventListener : function() {}};\n" +
   "    if (typeof Elm === \"undefined\") { throw \"elm-io config error: Elm is not defined. Make sure you call elm-io with a real Elm output file\"}\n" +
   "    if (typeof Elm.Main === \"undefined\" ) { throw \"Elm.Main is not defined, make sure your module is named Main.\" };\n" +
   "    var worker = Elm.worker(Elm.Main);\n" +
