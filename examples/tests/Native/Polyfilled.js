aa1b976d36b38f8f6eef041f68d6e1081b861a04
Stoeffel <schtoeffel@gmail.com>
2016-11-07 14:53:56 +0100

    adds failing test

---
+++ examples/tests/Native/Polyfilled.js
@@ -0,0 +1,10 @@
+if (!window) {
+    console.error('window not polyfilled');
+    process.exit(1);
+}
+var _rtfeldman$node_test_runner$Native_Polyfilled = function()
+{
+    return {
+      identity : function(x) { return x; }
+    };
+}();
