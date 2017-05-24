510ee51c060d9302f0ffa142d5fbdab9663bd9b9
Richard Feldman <richard.t.feldman@gmail.com>
2017-05-24 00:54:26 -0700

    Add link to getting started guidelines.

--- templates/tests/Example.elm
+++ templates/tests/Example.elm
@@ -7,4 +7,4 @@ import Fuzz exposing (list, int, string)
 
 suite : Test
 suite =
-    todo "Implement our first test."
+    todo "Implement our first test. See http://package.elm-lang.org/packages/elm-community/elm-test/latest for how to do this!"
