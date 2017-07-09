6034a7095eaebe048a8b619091467a070686eebc
Richard Feldman <richard.t.feldman@gmail.com>
2017-07-09 00:32:47 -0400

    Prettier on ci.js, fixed test incomplete bug

--- tests/ci.js
+++ tests/ci.js
@@ -1,10 +1,10 @@
-require('shelljs/global');
-var _ = require('lodash');
-var fs = require('fs-extra');
-var path = require('path');
-var spawn = require('cross-spawn');
+require("shelljs/global");
+var _ = require("lodash");
+var fs = require("fs-extra");
+var path = require("path");
+var spawn = require("cross-spawn");
 
-var filename = __filename.replace(__dirname + '/', '');
+var filename = __filename.replace(__dirname + "/", "");
 var elmTest = "elm-test";
 
 function run(testFile) {
@@ -15,14 +15,36 @@ function run(testFile) {
     var cmd = [elmTest, testFile].join(" ");
 
     echo("Running: " + cmd);
-    return exec(cmd).code
+    return exec(cmd).code;
+  }
+}
+
+function assertTestIncomplete(testfile) {
+  var code = run(testfile);
+  if (code !== 3) {
+    exec(
+      "echo " +
+        filename +
+        ": error: " +
+        (testfile ? testfile + ": " : "") +
+        "expected tests to exit with INCOMPLETE exit code, not exit code" +
+        code +
+        " >&2"
+    );
+    exit(1);
   }
 }
 
 function assertTestFailure(testfile) {
   var code = run(testfile);
   if (code < 2) {
-    exec('echo ' + filename + ': error: ' + (testfile ? testfile + ': ' : '') + 'expected tests to fail >&2');
+    exec(
+      "echo " +
+        filename +
+        ": error: " +
+        (testfile ? testfile + ": " : "") +
+        "expected tests to fail >&2"
+    );
     exit(1);
   }
 }
@@ -30,38 +52,54 @@ function assertTestFailure(testfile) {
 function assertTestSuccess(testFile) {
   var code = run(testFile);
   if (code !== 0) {
-    exec('echo ' + filename + ': ERROR: ' + (testFile ? testFile + ': ' : '') + 'Expected tests to pass >&2');
+    exec(
+      "echo " +
+        filename +
+        ": ERROR: " +
+        (testFile ? testFile + ": " : "") +
+        "Expected tests to pass >&2"
+    );
     exit(1);
   }
 }
 
-echo(filename + ': Uninstalling old elm-test...');
-exec('npm remove --ignore-scripts=false --global ' + elmTest);
+echo(filename + ": Uninstalling old elm-test...");
+exec("npm remove --ignore-scripts=false --global " + elmTest);
 
-echo(filename + ': Installing elm-test...');
-exec('npm link --ignore-scripts=false');
+echo(filename + ": Installing elm-test...");
+exec("npm link --ignore-scripts=false");
 
 var binaryExtension = process.platform === "win32" ? ".exe" : "";
-var interfacePath = path.resolve(path.join(__dirname, "..", "bin", "elm-interface-to-json" + binaryExtension));
+var interfacePath = path.resolve(
+  path.join(__dirname, "..", "bin", "elm-interface-to-json" + binaryExtension)
+);
 if (!fs.existsSync(interfacePath)) {
-  echo(filename + ': Failed because elm-interface-to-json was not found at ' + interfacePath);
+  echo(
+    filename +
+      ": Failed because elm-interface-to-json was not found at " +
+      interfacePath
+  );
   exit(1);
 }
 
-echo(filename + ': Verifying installed elm-interface-to-json...');
+echo(filename + ": Verifying installed elm-interface-to-json...");
 var interfaceExitCode = spawn.sync(interfacePath, ["--help"]).status;
 
 if (interfaceExitCode !== 0) {
-  echo(filename + ': Failed because `elm-interface-to-json` is present, but `elm-interface-to-json --help` returned with exit code ' + interfaceExitCode);
+  echo(
+    filename +
+      ": Failed because `elm-interface-to-json` is present, but `elm-interface-to-json --help` returned with exit code " +
+      interfaceExitCode
+  );
   exit(1);
 }
 
-echo(filename + ': Verifying installed elm-test version...');
-exec(elmTest + ' --version');
+echo(filename + ": Verifying installed elm-test version...");
+exec(elmTest + " --version");
 
-echo('### Testing elm-test on example/');
+echo("### Testing elm-test on example/");
 
-cd('example');
+cd("example");
 
 assertTestSuccess(path.join("tests", "*Pass*"));
 assertTestFailure(path.join("tests", "*Fail*"));
@@ -75,36 +113,40 @@ ls("tests/*.elm").forEach(function(testToRun) {
     echo("\n### Testing " + testToRun + " (expecting it to fail)\n");
     assertTestFailure(testToRun);
   } else {
-    echo("Tried to run " + testToRun + " but it has an invalid filename; node-test-runner tests should fit the pattern \"*Passing.elm\" or \"*Failing.elm\"");
+    echo(
+      "Tried to run " +
+        testToRun +
+        ' but it has an invalid filename; node-test-runner tests should fit the pattern "*Passing.elm" or "*Failing.elm"'
+    );
     process.exit(1);
   }
 });
 
-cd('..');
+cd("..");
 
-echo('### Testing elm-test init && elm-test');
-rm('-Rf', 'tmp');
-mkdir('-p', 'tmp');
-cd('tmp');
-exec(elmTest + ' init --yes');
-assertTestFailure();
+echo("### Testing elm-test init && elm-test");
+rm("-Rf", "tmp");
+mkdir("-p", "tmp");
+cd("tmp");
+exec(elmTest + " init --yes");
+assertTestIncomplete();
 
-cd('..');
+cd("..");
 
-echo('\n### Testing elm-test init on a non-empty directory\n');
-rm('-Rf', 'tmp');
-cp('-R', 'tests/init-test', 'tmp');
-cd('tmp');
-exec(elmTest + ' init --yes');
-assertTestFailure();
+echo("\n### Testing elm-test init on a non-empty directory\n");
+rm("-Rf", "tmp");
+cp("-R", "tests/init-test", "tmp");
+cd("tmp");
+exec(elmTest + " init --yes");
+assertTestIncomplete();
 
-cd('..');
-rm('-Rf', 'tmp');
+cd("..");
+rm("-Rf", "tmp");
 
-echo('');
-echo(filename + ': Everything looks good!');
-echo('                                                            ');
-echo('  __   ,_   _  __,  -/-     ,         __   __   _   ,    ,  ');
-echo('_(_/__/ (__(/_(_/(__/_    _/_)__(_/__(_,__(_,__(/__/_)__/_)_');
-echo(' _/_                                                        ');
-echo('(/                                                          ');
+echo("");
+echo(filename + ": Everything looks good!");
+echo("                                                            ");
+echo("  __   ,_   _  __,  -/-     ,         __   __   _   ,    ,  ");
+echo("_(_/__/ (__(/_(_/(__/_    _/_)__(_/__(_,__(_,__(/__/_)__/_)_");
+echo(" _/_                                                        ");
+echo("(/                                                          ");
