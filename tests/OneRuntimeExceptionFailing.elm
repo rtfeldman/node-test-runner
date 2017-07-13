ab7fec64da8710739883525a507cb0927e10aa1c
Richard Feldman <richard.t.feldman@gmail.com>
2017-07-13 17:41:46 -0400

    Fix MVar blocked operation

--- tests/OneRuntimeExceptionFailing.elm
+++ tests/OneRuntimeExceptionFailing.elm
@@ -8,4 +8,9 @@ suite : Test
 suite =
     test "intentional failure" <|
         \() ->
-            Debug.crash "This is intentionally failing with a runtime exception!"
+            -- If we don't have this condition, compiler blocks indefinitely on
+            -- an MVar operation.
+            if True then
+                Debug.crash "This is intentionally failing with a runtime exception!"
+            else
+                Expect.pass
