4ecd1068f31e221104454b85d6c0b2c8b8f9902c
Richard Feldman <richard.t.feldman@gmail.com>
2017-09-11 04:10:09 -0400

    Use Unix pipes instead of process.send

--- templates/after.js
+++ templates/after.js
@@ -14,16 +14,35 @@ if (potentialModuleNames.length !== 1) {
   process.exit(1);
 }
 
+var net = require("net"),
+  client = net.createConnection("/tmp/elm_test.sock");
+
+client.on("error", function(error) {
+  console.error(error);
+  client.end();
+  process.exit(1);
+});
+
+client.setEncoding("utf8");
+client.setNoDelay(true);
+
 var testModule = Elm.Test.Generated[potentialModuleNames[0]];
 
 // Run the Elm app.
 var app = testModule.worker({ seed: initialSeed, report: report });
 
-// Use ports for inter-process communication.
-app.ports.send.subscribe(function(msg) {
-  process.send(msg);
+client.on("data", function(msg) {
+  app.ports.receive.send(JSON.parse(msg));
 });
 
-process.on("message", function(msg) {
-  app.ports.receive.send(msg);
+// Use ports for inter-process communication.
+app.ports.send.subscribe(function(msg) {
+  // We do this in order to prevent a bug where two back-to-back-json
+  // messages get passed, resulting in the supervisor trying to parse
+  // "{ ...json object 1... }{ ...json object 2... }" which won't parse.
+  // By appending a comma, and having the supervisor strip the
+  // trailing comma and wrap the whole thing in [], the above becomes
+  // the valid JSON string "[{ ...json object 1... },{ ...json object 2... }]"
+  // and all is well!
+  client.write(msg + ",");
 });
