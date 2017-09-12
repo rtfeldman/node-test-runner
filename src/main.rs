1fc7c2e513a9f32a110c878b072cb9235b175ebb
Richard Feldman <richard.t.feldman@gmail.com>
2017-09-11 23:47:33 -0400

    Get paths to elm-package and elm-make

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate clap;
 extern crate term;
 use clap::{App, Arg};
+use std::path::{Path, PathBuf};
 
 fn main() {
     let version = "0.18.10";
@@ -23,6 +24,14 @@ fn main() {
                 .takes_value(true),
         )
         .arg(
+            Arg::with_name("compiler")
+                .short("c")
+                .long("compiler")
+                .value_name("PATH_TO_COMPILER")
+                .help("Run tests using this elm-make executable for the compiler")
+                .takes_value(true),
+        )
+        .arg(
             Arg::with_name("TESTFILES")
                 .help("Run TESTFILES, for example ")
                 .multiple(true)
@@ -40,6 +49,10 @@ fn main() {
     let seed: Option<i32> = parse_or_die("--seed", args.value_of("seed"));
     let fuzz: Option<i32> = parse_or_die("--fuzz", args.value_of("fuzz"));
 
+    let (path_to_elm_package, path_to_elm_make) =
+        binary_paths_from_compiler(args.value_of("compiler"));
+
+
     println!("Value for seed: {}", seed.unwrap_or(9).to_string());
     println!("Value for fuzz: {}", fuzz.unwrap_or(9).to_string());
     println!(
@@ -51,14 +64,39 @@ fn main() {
     );
 }
 
+// Return a path to the elm-package binary, and optionally one to elm-make as well.
+fn binary_paths_from_compiler(arg: Option<&str>) -> (PathBuf, Option<PathBuf>) {
+    return match arg {
+        Some(compiler) => {
+            let valid_compiler_path = match PathBuf::from(compiler).canonicalize() {
+                Ok(canonicalized) => if canonicalized.is_dir() {
+                    panic!(
+                        "The --compiler flag must be given a path to an elm-make executable,\
+                         not a directory."
+                    );
+                } else {
+                    canonicalized
+                },
+
+                Err(_) => {
+                    panic!("The --compiler flag must be given a path to an elm-make executable.");
+                }
+            };
+
+            (
+                valid_compiler_path.with_file_name("elm-package"),
+                Some(valid_compiler_path),
+            )
+        }
+        None => (PathBuf::from("elm-package"), None),
+    };
+}
+
 // Turn the given Option<&str> into an Option<i32>, or else die and report the invalid argument.
 fn parse_or_die(arg_name: &str, val: Option<&str>) -> Option<i32> {
     return val.map(|str| match str.parse::<i32>() {
         Ok(num) => num,
-        Err(_) => {
-            println!("Invalid {} value: {}", arg_name, str);
-            ::std::process::exit(1);
-        }
+        Err(_) => panic!("Invalid {} value: {}", arg_name, str),
     });
 }
 
