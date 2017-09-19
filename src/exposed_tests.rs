7fd80dd154651d3db69d08c2ca3dfcc6b0859298
Richard Feldman <richard.t.feldman@gmail.com>
2017-09-19 08:51:55 -0400

    Finish getting parser to compile.

--- src/exposed_tests.rs
+++ src/exposed_tests.rs
@@ -1,6 +1,7 @@
 // Determine which values of type Test are exposed from a given module.
 
 use std::fs::File;
+use std::iter::FromIterator;
 use std::io::{BufReader, BufRead};
 use io;
 use std::path::{Path, PathBuf};
@@ -20,16 +21,16 @@ pub fn filter_exposing(
     tests: &HashSet<String>,
     module_name: &str,
 ) -> Result<(String, HashSet<String>), Problem> {
-    let new_tests: HashSet<String> = match read_exposing(path)? {
-        // None for exposed_values means "the module was exposing (..), so keep everything"
-        None => tests.clone(),
+    let exposing = read_exposing(path)?;
+    let new_tests: HashSet<String> = if exposing.contains("..") && exposing.len() == 1 {
+        // the module was exposing (..), so keep everything
+        tests.clone()
+    } else {
         // Only keep the tests that were exposed.
-        Some(exposed_values) => {
-            exposed_values
-                .intersection(&tests)
-                .cloned()
-                .collect::<HashSet<String>>()
-        }
+        exposing
+            .intersection(&tests)
+            .cloned()
+            .collect::<HashSet<String>>()
     };
 
     if new_tests.len() < tests.len() {
@@ -50,7 +51,7 @@ enum ParsedLineResult {
     Exposing(HashSet<String>, bool),
 }
 
-fn read_exposing(path: &Path) -> Result<Option<HashSet<String>>, Problem> {
+fn read_exposing(path: &Path) -> Result<HashSet<String>, Problem> {
     let file = File::open(path).map_err(|err| {
         Problem::OpenFileToReadExports(path.to_path_buf(), err)
     })?;
@@ -58,40 +59,216 @@ fn read_exposing(path: &Path) -> Result<Option<HashSet<String>>, Problem> {
     let mut line = String::new();
     let mut exposing: HashSet<String> = HashSet::new();
 
+    // how many levels deep in block comments we are.
+    let mut block_comment_depth = 0;
+
+    // if the file does not have a module line
+    let mut is_missing_module_name = false;
+
+    // if the module line has been read
+    let mut has_module_line_been_read = false;
+
+    let mut is_reading_module_name = false;
+    let mut is_reading_exports = false;
+    let mut is_between_parens = false;
+
+    // number of open/closed parens seen so far
+    let mut open_parens_seen = 0;
+    let mut closed_parens_seen = 0;
+
+    // data between exposing brackets
+    let mut data = String::new();
+
     loop {
         reader.read_line(&mut line).map_err(|err| {
             Problem::OpenFileToReadExports(path.to_path_buf(), err)
         })?;
 
-        match parse_line(&line) {
-            Ok(ParsedLineResult::AllExposed) => {
-                return Ok(None);
-            }
-            Ok(ParsedLineResult::Exposing(new_exposing, is_done)) => {
-                for val in new_exposing {
-                    exposing.insert(val);
-                }
+        let (line_without_comments, new_block_comment_depth) =
+            strip_comments(&line, block_comment_depth);
 
-                if is_done {
-                    return Ok(Some(exposing));
+        line = line_without_comments.to_owned();
+        block_comment_depth = new_block_comment_depth;
+
+        if line.is_empty() {
+            continue;
+        }
+
+        // if we haven't started reading the first line
+        if !has_module_line_been_read {
+            let new_line = remove_module_declaration(&line);
+            if new_line == line {
+                // We did not find a module to remove, meaning we found content before the module
+                // declaration. Error!
+                return Err(Problem::MissingModuleDeclaration(path.to_path_buf()));
+            }
+        } else {
+            // We found and successfully removed the module declaration.
+            has_module_line_been_read = true;
+            is_reading_module_name = true;
+
+            if line.is_empty() {
+                continue;
+            }
+        }
+
+        // if we are still reading the module line, find "exposing" and remove it.
+        if is_reading_module_name {
+            match remove_exposing(&line) {
+                Some(line_without_exposing) => {
+                    line = line_without_exposing;
+
+                    is_reading_module_name = false;
+                    is_reading_exports = true;
+
+                    if line.is_empty() {
+                        continue;
+                    }
+                }
+                None => {
+                    continue;
                 }
             }
-            Err(_) => {
-                return Err(Problem::ParseError(path.to_path_buf()));
+        }
+
+        // if we are actually reading the exports
+        if is_reading_exports {
+            // remove everything before the open paren
+            match remove_before_open_paren(&line) {
+                Some(line_without_open_paren) => {
+                    open_parens_seen += 1;
+                    is_reading_exports = false;
+                    is_between_parens = true;
+
+                    line = line_without_open_paren;
+                }
+                None => {
+                    continue;
+                }
+            }
+        }
+
+        // if we're before the final bracket
+        if is_between_parens {
+            // TODO this is probably inefficient
+            let new_open_parens_seen = line.split("(").count();
+            let new_closed_parens_seen = line.split(")").count();
+
+            closed_parens_seen += new_closed_parens_seen;
+            open_parens_seen += new_open_parens_seen;
+
+            data += &line;
+
+            if closed_parens_seen == open_parens_seen {
+                return Ok(split_exposing(&data));
             }
         }
     }
 }
 
-fn parse_line(line: &str) -> Result<ParsedLineResult, ()> {
-    return Err(());
+
+// remove everything before the open paren
+fn remove_before_open_paren(line: &str) -> Option<String> {
+    match line.find("(") {
+        Some(first_paren_index) => {
+            let line_length = line.len();
+            let start_index = first_paren_index + 1;
+
+            unsafe { Some(line.slice_unchecked(start_index, line_length).to_owned()) }
+        }
+        None => None,
+    }
+}
+
+
+#[cfg(test)]
+mod test_remove_open_paren {
+    use super::*;
+
+    #[test]
+    fn no_exposing() {
+        assert_eq!(None, remove_before_open_paren("blah"));
+    }
+
+    #[test]
+    fn starts_with_paren() {
+        assert_eq!(
+            Some(" foo bar".to_owned()),
+            remove_before_open_paren("( foo bar")
+        );
+    }
+
+    #[test]
+    fn ends_with_paren() {
+        assert_eq!(
+            Some("".to_owned()),
+            remove_before_open_paren(" blah, etc (")
+        );
+    }
+
+    #[test]
+    fn paren_surrounded() {
+        assert_eq!(
+            Some(" etc, blah ".to_owned()),
+            remove_before_open_paren(" foo, bar ( etc, blah ")
+        );
+    }
+}
+
+fn remove_exposing(line: &str) -> Option<String> {
+    match line.find("exposing") {
+        Some(exposing_index) => {
+            let line_length = line.len();
+            let after_index = exposing_index + 8;
+
+            let before_exposing = unsafe { line.slice_unchecked(0, exposing_index) };
+            let after_exposing = unsafe { line.slice_unchecked(after_index, line_length) };
+
+            return Some(before_exposing.to_owned() + after_exposing);
+        }
+        None => None,
+    }
+}
+
+#[cfg(test)]
+mod test_remove_exposing {
+    use super::*;
+
+    #[test]
+    fn no_exposing() {
+        assert_eq!(None, remove_exposing("blah"));
+    }
+
+    #[test]
+    fn starts_with_exposing() {
+        assert_eq!(
+            Some(" foo, bar".to_owned()),
+            remove_exposing("exposing foo, bar")
+        );
+    }
+
+    #[test]
+    fn ends_with_exposing() {
+        assert_eq!(
+            Some(" blah, etc ".to_owned()),
+            remove_exposing(" blah, etc exposing")
+        );
+    }
+
+    #[test]
+    fn exposing_surrounded() {
+        assert_eq!(
+            Some(" foo, bar  etc, blah ".to_owned()),
+            remove_exposing(" foo, bar exposing etc, blah ")
+        );
+    }
 }
 
 
 /* Remove all the comments from the line,
    and also return whether we are still in a comment block.
 */
-fn strip_comments(original_line: &str, original_block_comment_depth: u32) -> (&str, u32) {
+fn strip_comments(original_line: &str, original_block_comment_depth: u32) -> (String, u32) {
     let mut line = original_line;
     let mut block_comment_depth = original_block_comment_depth;
 
@@ -164,10 +341,10 @@ fn strip_comments(original_line: &str, original_block_comment_depth: u32) -> (&s
             (None, None) => {
                 if block_comment_depth > 0 {
                     // All of this is a comment, so throw it all out.
-                    return ("", block_comment_depth);
+                    return ("".to_owned(), block_comment_depth);
                 } else {
                     // None of this is a comment, so keep it all.
-                    return (line, block_comment_depth);
+                    return (line.to_owned(), block_comment_depth);
                 }
             }
         }
@@ -182,7 +359,7 @@ mod test_strip_comments {
     #[test]
     fn strips_inline_comments() {
         assert_eq!(
-            ("module Foo ", 0),
+            ("module Foo ".to_owned(), 0),
             strip_comments("module Foo -- blah blah whatever ", 0)
         );
     }
@@ -191,7 +368,7 @@ mod test_strip_comments {
     fn single_line_comment_inside_block_comment() {
         for depth in 1..3 {
             assert_eq!(
-                ("", depth),
+                ("".to_owned(), depth),
                 strip_comments(" Foo -- single line comment inside block comment", depth)
             );
         }
@@ -201,7 +378,7 @@ mod test_strip_comments {
     fn nested_block_comment() {
         for depth in 1..3 {
             assert_eq!(
-                ("", depth + 1),
+                ("".to_owned(), depth + 1),
                 strip_comments(" Bar {- start of nested block comment", depth)
             );
         }
@@ -211,7 +388,7 @@ mod test_strip_comments {
     fn inside_block_comment() {
         for depth in 1..3 {
             assert_eq!(
-                ("", depth),
+                ("".to_owned(), depth),
                 strip_comments("stuff inside block comment", depth)
             );
         }
@@ -221,7 +398,7 @@ mod test_strip_comments {
     fn end_of_block_comment() {
         let depth = 1;
         assert_eq!(
-            (" bar baz", depth - 1),
+            (" bar baz".to_owned(), depth - 1),
             strip_comments("end of block comment -} bar baz", depth)
         );
     }
@@ -230,7 +407,7 @@ mod test_strip_comments {
     fn end_of_nested_block_comment() {
         for depth in 2..3 {
             assert_eq!(
-                ("", depth - 1),
+                ("".to_owned(), depth - 1),
                 strip_comments("end of block comment -} bar baz", depth)
             );
         }
@@ -290,15 +467,15 @@ mod test_remove_module_declaration {
     }
 }
 
-fn split_exposing(line: &str) -> HashSet<&str> {
+fn split_exposing(line: &str) -> HashSet<String> {
     match line.rfind(")") {
         Some(close_paren_index) => {
             // We know these indices will be okay because we got them from find()
             unsafe {
                 line.slice_unchecked(0, close_paren_index)
                     .split(",")
-                    .map(str::trim)
-                    .collect::<HashSet<&str>>()
+                    .map(|string| string.trim().to_owned())
+                    .collect::<HashSet<String>>()
             }
         }
         None => HashSet::new(),
@@ -312,7 +489,10 @@ mod test_split_exposing {
     #[test]
     fn none() {
         assert_eq!(
-            [""].iter().cloned().collect::<HashSet<&str>>(),
+            vec![""]
+                .into_iter()
+                .map(|string| string.to_owned())
+                .collect::<HashSet<String>>(),
             split_exposing("   )  ")
         );
     }
@@ -320,7 +500,10 @@ mod test_split_exposing {
     #[test]
     fn one() {
         assert_eq!(
-            ["one"].iter().cloned().collect::<HashSet<&str>>(),
+            vec!["one"]
+                .into_iter()
+                .map(|string| string.to_owned())
+                .collect::<HashSet<String>>(),
             split_exposing("one ) ")
         );
     }
@@ -328,7 +511,10 @@ mod test_split_exposing {
     #[test]
     fn two() {
         assert_eq!(
-            ["one", "two"].iter().cloned().collect::<HashSet<&str>>(),
+            vec!["one", "two"]
+                .into_iter()
+                .map(|string| string.to_owned())
+                .collect::<HashSet<String>>(),
             split_exposing(" one , two ) ")
         );
     }
@@ -336,123 +522,11 @@ mod test_split_exposing {
     #[test]
     fn three() {
         assert_eq!(
-            ["one", "two", "three"]
-                .iter()
-                .cloned()
-                .collect::<HashSet<&str>>(),
+            vec!["one", "two", "three"]
+                .into_iter()
+                .map(|string| string.to_owned())
+                .collect::<HashSet<String>>(),
             split_exposing(" one , two ,    three ) ")
         );
     }
 }
-
-
-
-// fn parse(reader: &mut BufReader) {
-//   // if we're currently in a comment
-//   let mut is_in_comment = false;
-//
-//   // if the file does not have a module line
-//   let mut is_missing_module_name = false;
-//
-//   // if we're done parsing
-//   let mut parsing_done = false;
-//
-//   // if the module line has been read
-//   let mut has_module_line_been_read = false;
-//
-//   let mut is_reading_module_name = false;
-//   let mut is_reading_exports = false;
-//   let mut is_between_parens = false;
-//
-//   // values that have been exposed
-//   let mut exposed_values = vec![];
-//
-//   // number of open/closed parens seen so far
-//   let mut open_parens_seen = 0;
-//   let mut closed_parens_seen = 0;
-//
-//   // data between exposing brackets
-//   let mut data = "";
-//
-//   fn parse_line(line: &mut str) {
-//     if parsing_done { return; }
-//
-//     is_in_comment = stripComments(line, isInComment);
-//     line = line.trim();
-//
-//     if line.is_empty() { return; }
-//
-//     // if we haven't started reading the first line
-//     if !has_module_line_been_read {
-//         let new_line = remove_module_declaration(line);
-//         if new_line == line {
-//         // We did not find a module to remove, meaning we found content before the module
-//         // declaration. Error!
-//           is_missing_module_name = true;
-//           parsing_done = true;
-//
-//           return;
-//         }
-//       } else {
-//             // We found and successfully removed the module declaration.
-//           has_module_line_been_read = true;
-//           is_reading_module_name = true;
-//
-//           if line.is_empty() {return;}
-//     }
-//
-//     // if we are still reading the module line
-//     if is_reading_module_name {
-//       match line.find("exposing") {
-//           Some(exposing_index) => {
-//             let line_length = line.len();
-//               let dest_index = exposing_index + 8;
-//                 unsafe {
-//                     line.slice_mut_unchecked(dest_index, line_length - dest_index);
-//                 }
-//               is_reading_module_name = false;
-//               is_reading_exports = true;
-//
-//               if line.is_empty() {return;}
-//           },
-//           None => { return; }
-//       }
-//     }
-//
-//     // if we are actually reading the exports
-//     if is_reading_exports {
-//         match line.find("(") {
-//             Some(first_paren_index) => {
-//               open_parens_seen += 1;
-//               is_reading_exports = false;
-//               is_between_parens = true;
-//
-//                 let line_length = line.len();
-//                   let dest_index = first_paren_index + 1;
-//
-//
-//                 unsafe {
-//                     line.slice_mut_unchecked(dest_index, line_length - dest_index);
-//                 }
-//             }
-//             None => { return; }
-//         }
-//     }
-//
-//     // if we're before the final bracket
-//     if is_between_parens {
-//       let new_open_parens_seen = line.split("(").len();
-//       let new_closed_parens_seen = line.split(")").len();
-//
-//       closed_parens_seen += new_closed_parens_seen;
-//       open_parens_seen += new_open_parens_seen;
-//
-//       data += line;
-//
-//       if closedBracketsSeen == openBracketsSeen {
-//         exposed_values = split_exposing(data);
-//         parsing_done = true;
-//       }
-//     }
-//   }
-// }
