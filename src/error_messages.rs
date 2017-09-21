2369775a1f46d7283d30272a71b37fcd47a65197
Richard Feldman <richard.t.feldman@gmail.com>
2017-09-21 07:25:42 -0400

    :skull: dead code

--- src/error_messages.rs
+++ src/error_messages.rs
@@ -1,5 +1,4 @@
 
-use std::path::PathBuf;
 use problems::Problem;
 use read_elmi;
 use files::ElmJsonError;
--- src/problems.rs
+++ src/problems.rs
@@ -1,5 +1,5 @@
 use std::io;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::collections::HashSet;
 use read_elmi;
 use files;
