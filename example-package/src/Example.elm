47a355795900b47765a88a2d670949bfe0519b8b
Aaron VonderHaar <av@noredink.com>
2018-05-30 21:53:37 -0700

    When testing package projects, add the necessary transitive dependencies

--- example-package/src/Example.elm
+++ example-package/src/Example.elm
@@ -1,5 +1,13 @@
 module Example exposing (..)
 
+-- Elm.Docs from elm/project-metadata-utils is imported because
+-- it has a dependency on elm/parser, which is not a dependency of
+-- node-test-runner.  This is done to test that the generated elm.json
+-- for package projects correctly includes any necessary transitive
+-- dependencies of the package being tested
+
+import Elm.Docs
+
 
 ultimateAnswer : Int
 ultimateAnswer =
