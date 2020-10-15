62a3bff1f5cfbdc163a2c9b046d5cab0ac04480a
Simon Lydell <simon.lydell@gmail.com>
2020-10-15 23:51:32 +0200

    Test that string replacement doesn’t mess with multiline strings

--- example-application/tests/TestsPassing.elm
+++ example-application/tests/TestsPassing.elm
@@ -1,4 +1,4 @@
-module TestsPassing exposing (testEqual, testFalse, testTrue)
+module TestsPassing exposing (..)
 
 import Expect
 import Something
@@ -27,3 +27,32 @@ testFalse =
         \() ->
             False
                 |> Expect.false "this should never fail!"
+
+
+{-| This _looks_ like the code that we replace in Generate.js,
+but it shouldn’t be touched. It won’t be because the compiled JS
+uses a single-line strings with `\n` escapes.
+
+notATest = testTrue
+
+-}
+code : String
+code =
+    """
+var $author$project$Test$Runner$Node$check = function (value) {
+  return $elm$core$Maybe$Nothing;
+};
+
+var $elm_explorations$test$Test$Internal$Batch = function (a) {
+  return {$: 'Batch', a: a};
+};
+"""
+
+
+testCode : Test
+testCode =
+    test "Multiline string is changed by mistake" <|
+        \() ->
+            code
+                |> Expect.equal
+                    "\nvar $author$project$Test$Runner$Node$check = function (value) {\n  return $elm$core$Maybe$Nothing;\n};\n\nvar $elm_explorations$test$Test$Internal$Batch = function (a) {\n  return {$: 'Batch', a: a};\n};\n"
