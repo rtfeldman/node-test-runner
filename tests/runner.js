575196a0a8f9fadf1af5ff213f83b303f6157335
Simon Lydell <simon.lydell@gmail.com>
2020-10-24 11:10:38 +0200

    Drop support for Node.js 8 (#461)
    * Drop support for Node.js 8
    
    * Fix weird registry for picomatch in package-lock.json
    
    This caused a test failure on Node.js 15.

--- tests/runner.js
+++ tests/runner.js
@@ -4,18 +4,6 @@ const Runner = require('../lib/Runner.js');
 const assert = require('assert');
 const path = require('path');
 
-// TODO: remove this when we drop node 8 support
-function assertThrowsAsync(fn, regExp) {
-  return fn()
-    .then(
-      () => () => {},
-      (e) => () => {
-        throw e;
-      }
-    )
-    .then((f) => assert.throws(f, regExp));
-}
-
 describe('Runner', () => {
   describe('getFirstLine', () => {
     it('gets first line of this file', () =>
@@ -35,18 +23,18 @@ describe('Runner', () => {
 
     it('fails on an empty file', () => {
       const p = path.join(__dirname, 'fixtures', 'empty');
-      return assertThrowsAsync(
-        () => Runner.getFirstLine(p),
-        `File ${p} is empty`
-      );
+      return assert.rejects(() => Runner.getFirstLine(p), {
+        name: 'Error',
+        message: `File ${p} is empty!`,
+      });
     });
 
     it('fails a non existant file', () => {
       const p = path.join(__dirname, 'fixtures', 'must-never-exist');
-      return assertThrowsAsync(
-        () => Runner.getFirstLine(p),
-        `ENOENT: no such file or directory`
-      );
+      return assert.rejects(() => Runner.getFirstLine(p), {
+        name: 'Error',
+        message: `ENOENT: no such file or directory, open '${p}'`,
+      });
     });
   });
 });
