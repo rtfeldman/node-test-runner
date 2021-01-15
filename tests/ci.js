51b80aad48f5b9ca4444e4b5628db7b02c9ce5ab
Simon Lydell <simon.lydell@gmail.com>
2021-01-15 21:16:58 +0100

    Add `elm-test make` tests

--- tests/ci.js
+++ tests/ci.js
@@ -115,6 +115,11 @@ describe('Testing elm-test on an example application', () => {
     const runResult = execElmTest([args], cwd);
     assertTestFailure(runResult);
   }).timeout(60000);
+
+  it('Should successfully run `elm-test make`', () => {
+    const runResult = execElmTest(['make'], cwd);
+    assertTestSuccess(runResult);
+  }).timeout(60000);
 });
 
 describe('Testing elm-test on an example package', () => {
@@ -130,6 +135,11 @@ describe('Testing elm-test on an example package', () => {
     const args = path.join('tests', '*Fail*.elm');
     const runResult = execElmTest([args], cwd);
     assertTestFailure(runResult);
+  }).timeout(60000);
+
+  it('Should successfully run `elm-test make`', () => {
+    const runResult = execElmTest(['make'], cwd);
+    assertTestSuccess(runResult);
   }).timeout(60000);
 });
 
