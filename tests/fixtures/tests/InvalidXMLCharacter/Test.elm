df360236dd257a4a047aa28fdabcecea548a8379
ilias <mail@ilias.xyz>
2023-02-14 16:03:54 +0100

    Add extra test-case

--- tests/fixtures/tests/InvalidXMLCharacter/Test.elm
+++ tests/fixtures/tests/InvalidXMLCharacter/Test.elm
@@ -6,7 +6,9 @@ import Test exposing (..)
 
 invalidCharacter : Test
 invalidCharacter =
-    describe "The junit reporter should not crash due to invalid control characters"
+    describe "The junit reporter should not crash due to invalid (for XML) characters in the output"
         [ test "backspace: \u{0008}" <|
+            \() -> Expect.pass
+        , test "escape: \u{001B}" <|
             \() -> Expect.pass
         ]
