e2bf3e4eb24a048cb8f1cf9c180252510b98de32
Juliano Solanho <juliano.solanho@gmail.com>
2025-11-17 18:13:24 -0300

    Fix mismatched app and test versions for indirect dependencies (#653)
    elm-test was running tests with indirect dependencies always at latest
    available version, regardless of whether the app under test had indirect
    dependencies pinned to an earlier version.
    
    This can lead to tests behaving different from real world apps.
    
    The reason is elm-solve-deps always picks the first acceptable version
    given to it in the versions array.
    
    This commit fixes the issue by ensuring indirect dependencies' versions
    are always sorted in this order:
    - the pinned version first
    - versions higher than the pinned version, in ascending order
    - versions lower than the pinned version, in descending order
    
    Co-authored-by: Simon Lydell <simon.lydell@gmail.com>

---
+++ tests/DependencyProvider.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const assert = require('assert');
+const {
+  prioritizePinnedIndirectVersion,
+} = require('../lib/DependencyProvider');
+
+describe('DependencyProvider', () => {
+  describe('prioritizePinnedIndirectVersion', () => {
+    const versions = ['1.0.5', '1.0.4', '1.0.3', '1.0.2', '1.0.1', '1.0.0'];
+
+    const testPinning = (pinnedVersion, expectedResult) => {
+      assert.deepStrictEqual(
+        prioritizePinnedIndirectVersion(versions, pinnedVersion),
+        expectedResult
+      );
+    };
+
+    it('retains order when no pinned indirect dependency', () => {
+      testPinning(undefined, versions);
+    });
+
+    it("retains order when pinned version doesn't exist", () => {
+      testPinning('1.0.6', versions);
+    });
+
+    it('retains order if already at latest', () => {
+      testPinning('1.0.5', versions);
+    });
+
+    it("prioritizes a version in the middle, if we're pinned to it", () => {
+      const expected = [
+        // first, try the pinned version
+        '1.0.3',
+        // then, try upgrading
+        '1.0.4',
+        '1.0.5',
+        // then, try downgrading
+        '1.0.2',
+        '1.0.1',
+        '1.0.0',
+      ];
+      testPinning('1.0.3', expected);
+    });
+
+    it("prioritizes first version, if we're pinned to it", () => {
+      testPinning('1.0.0', [...versions].sort());
+    });
+  });
+});
